aad45d5143dbbed60dd692f5f410ad2ec0942260
kcameron37 <kcameron37@my.bcit.ca>
2023-03-21 21:46:44 -0700

    add navbar component

---
+++ components/navbar/index.js
@@ -0,0 +1,19 @@
+import styles from '../styles/navbar.module.css' 
+import Head from 'next/head'
+import Image from 'next/image'
+import React from 'react'
+
+
+export default function AboutUs() {
+    return (
+       <>
+       <main className={styles.main}>
+          
+            
+         <body className={styles.body}>
+         </body>
+         </main>
+       </>
+     )
+   }
+   
