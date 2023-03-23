cb823472b1f44034fa75f2eda0f2fcccafb4c1f9
kcameron37 <kcameron37@my.bcit.ca>
2023-03-23 09:58:47 -0700

    updates

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import Navbar from '@/components/navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+
+
 
 export default function Home() {
   return (
@@ -17,7 +17,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         
-        <Navbar/>
+       
        
       </main>
     </>
