738e1673e5817c4d38e8277f1322d46adec14302
kcameron37 <kcameron37@my.bcit.ca>
2023-03-23 10:05:55 -0700

    title update

--- pages/warning.js
+++ pages/warning.js
@@ -8,7 +8,7 @@ export default function Warning() {
     return (
        <>
          <Head>
-        <title>Open Doors</title>
+        <title>Open Doors - Unlocking a better tomorrow</title>
         <meta name="description" content="Unlocking a better tomorrow" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
