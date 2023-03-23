df35bcd14c79b7bcb8d710c774c82a70fc9d3ba2
kcameron37 <kcameron37@my.bcit.ca>
2023-03-23 09:41:18 -0700

    navbar updates

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -7,19 +7,7 @@ import Link from 'next/link'
 export default function Navbar() {
     return (
        <>
-       <main className={styles.main}>
-          
-       <nav className="navbar">
-      <div className="logo">
-        <Link href="/">
-          <p>Logo</p>
-        </Link>
-      </div>
-      <div className="menu-icon">
-        
-      </div>    
-     </nav> 
-         </main>
+     
        </>
      )
    }
