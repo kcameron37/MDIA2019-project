e96e30580e074d02ed107e14eaa9f186f5eea1ec
kcameron37 <115664906+kcameron37@users.noreply.github.com>
2023-04-10 21:12:15 -0700

    Home animation

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,19 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const [showLogo, setShowLogo] = useState(true);
-  const [backgroundColor, setBackgroundColor] = useState('#EAD9C5');
+  const [animationClass, setAnimationClass] = useState('');
 
   useEffect(() => {
     const timer = setTimeout(() => {
+      setAnimationClass(styles.slideDown);
       setShowLogo(false);
-      setBackgroundColor('#33718A');
     }, 6000);
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <div className={styles.container} style={{ backgroundColor }}>
+    <div className={`${styles.container} ${animationClass}`}>
       {showLogo ? (
         <Image
           src="/logo/front-logo.png"
@@ -28,13 +28,13 @@ export default function Home() {
       ) : (
         <>
           <div className={styles.homeImage}/>
-                      <div className={styles.text}>
+                  <div className={styles.text}>
             <div className={styles.header}>
-          <h1 className={styles.header}>Welcome to Open Doors!</h1>
-          </div>
-          <div className={styles.header}>
-          <p className={styles.paragraph}>We are here to help,<br></br> so let's get started</p>
-          </div>
+              <h1 className={styles.header}>Welcome to Open Doors!</h1>
+            </div>
+            <div className={styles.header}>
+              <p className={styles.paragraph}>We are here to help,<br></br> so let's get started</p>
+            </div>
           </div>
         </>
       )}
