732d88a91455c0c47b9a4fe969be0d8bd1d3c657
kcameron37 <115664906+kcameron37@users.noreply.github.com>
2023-04-10 20:12:42 -0700

    index page updates

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,45 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
-import Navbar from '@/components/Navbar/'
-
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [showLogo, setShowLogo] = useState(true);
+  const [backgroundColor, setBackgroundColor] = useState('#33718A');
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLogo(false);
+      setBackgroundColor('white');
+    }, 6000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <>
-      <Head>
-        <title>Open Doors - Unlocking a better tomorrow</title>
-        <meta name="description" content="Unlocking a better tomorrow" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      <main className={styles.main}>
-        <Navbar/>
-
-
-      </main>
-    </>
-  )
+    <div className={styles.container} style={{ backgroundColor }}>
+      {showLogo ? (
+        <Image
+          src="/logo/front-logo.png"
+          alt="Logo"
+          className={styles.logo}
+          width={414}
+          height={500}
+        />
+      ) : (
+        <>
+          <div className={styles.homeImage}>
+          <Image
+          src="/images/home.png"
+          alt="Logo"
+          className={styles.logo}
+          width={414}
+          height={414}
+        />
+          </div>
+          <h1 className={styles.header}>Welcome to Open Doors!</h1>
+          <p className={styles.paragraph}>We are here to help, so let's get started</p>
+        </>
+      )}
+    </div>
+  );
 }
