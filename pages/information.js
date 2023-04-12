21d41a7c7da82071a795c074ea126465c04e157f
kcameron37 <115664906+kcameron37@users.noreply.github.com>
2023-04-12 15:13:34 -0700

    add Head section to info page

--- pages/information.js
+++ pages/information.js
@@ -5,6 +5,13 @@ import Head from 'next/head';
 export default function Information() {
   return (
     <>
+     <Head>
+        <title>Open Doors</title>
+        <meta name="description" content="Unlocking a better tomorrow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <div className={styles.main}>
       <div className={styles.infoBackground}>
         <Image
           src="/images/info-background.png"
@@ -12,6 +19,7 @@ export default function Information() {
           width={414}
           height={894}
         />
+      </div>
       </div>
     </>
   );
