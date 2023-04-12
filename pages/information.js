575ec8d8908003c501731808d4abc2e36f155e0b
kcameron37 <115664906+kcameron37@users.noreply.github.com>
2023-04-11 21:25:05 -0700

    info page updates

--- pages/information.js
+++ pages/information.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Head from 'next/head';
 
 export default function Information() {
+  
   return (
-    <div>
-      <h1>Information Page</h1>
-    </div>
+    <>
+      <Head>
+        <title>Open Doors</title>
+        <meta name="description" content="Unlocking a better tomorrow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      
+      <main className={styles.main}>
+        
+      </main>
+     
+    </>
   );
-};
-
+}
