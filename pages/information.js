9d3ebc96a5175f073860f7f37e1c1795d0af17da
kcameron37 <115664906+kcameron37@users.noreply.github.com>
2023-04-12 14:57:56 -0700

    info page images

--- pages/information.js
+++ pages/information.js
@@ -1,24 +1,18 @@
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/information.module.css';
 import Head from 'next/head';
 
 export default function Information() {
-  
   return (
     <>
-      <Head>
-        <title>Open Doors</title>
-        <meta name="description" content="Unlocking a better tomorrow" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      
-      <main className={styles.main}>
-        
-      </main>
-     
+      <div className={styles.infoBackground}>
+        <Image
+          src="/images/info-background.png"
+          alt="Info"
+          width={414}
+          height={894}
+        />
+      </div>
     </>
   );
 }
