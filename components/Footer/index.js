b879ba5fc8dae3fb36333b5a979f62aacc46ea0c
EricQian01 <ericqian1203@gmail.com>
2023-04-20 14:20:56 -0700

    Nav Bar Icons

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,24 +1,22 @@
 import Link from 'next/link';
 import styles from './Footer.module.css';
 import Button from '@/components/Button';
+import Image from 'next/image';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <nav className={styles.nav}>
-        <ul className={styles.navList}>
-          <li className={styles.navListItem}>
-            <Link href="/">
-              <p>Home</p>
-            </Link>
-          </li>
-          <li className={styles.navListItem}>
-            <Link href="/support">
-              <p>Support</p>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <Link href="/">
+        <Image src="/icons/home-button.svg" height={50} width={50} />
+      </Link>
+
+      <Link href="/quiz1">
+        <Image src="/icons/quiz-button.svg" height={50} width={50} />
+      </Link>
+
+      <Link href="/support">
+        <Image src="/icons/help-button.svg" height={50} width={50} />
+      </Link>
     </footer>
   );
 }
