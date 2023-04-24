988d688d2b0bf6fc867f2b84a60af1dcba346985
Johnathan <johnathan.shang@gmail.com>
2023-04-24 14:43:03 -0700

    adjusted popup layout

--- components/popUp/index.js
+++ components/popUp/index.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styles from './popup.module.css';
+import Image from 'next/image';
 
 
 export default function PopUp() {
@@ -10,13 +11,13 @@ export default function PopUp() {
       {popup ? (
         <div className={styles.container}>
           <div className={styles.popup__area}>
-            <img src="/logo/front-logo.png"
-            alt="Warning" 
-            className={styles.warningImage}
-             />
+            <Image
+              src="/logo/front-logo.png"
+              width={300}
+              height={200}
+            />
             <h1 className={styles.warningText}>Warning</h1>
-            
-            <p className={styles.text}>If you require immediate assistance, please contact emergency services by dialing 911  </p>
+            <p>If you require immediate assistance, please contact emergency services by dialing 911  </p>
             <h3 className={styles.close} onClick={() => setPopup(false)}>Close</h3>
           </div>
         </div>
