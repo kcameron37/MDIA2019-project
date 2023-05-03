0bb2eca8fcd79afc533eb295bcf6b9c4299b4617
kcameron37 <115664906+kcameron37@users.noreply.github.com>
2023-05-03 14:22:56 -0700

    data updates

---
+++ data/data.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    ageGroup: "Under 18",
+    name: "Coventent House Youth Crisis Program",
+    website:
+      "https://www.covenanthousebc.org/programs-services/housing/crisis-program/",
+    phoneNumber: "1-877-685-7474",
+    address: "1280 Seymour St, Vancouver, BC V6B 3N9",
+    description:
+      "At Covenant House’s Crisis Program, we welcome young people, aged 16 to 24, who are at risk of, or are experiencing, homelessness. Our first priority is to meet young people’s basic needs, a safe space to sleep, nutritious food, warm clothing and showers",
+  },
+];
+
+export default data;
