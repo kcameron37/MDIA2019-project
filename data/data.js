7c3f608aa00c393faf6d5dd371653658aea9e73b
kcameron37 <115664906+kcameron37@users.noreply.github.com>
2023-05-03 14:32:39 -0700

    data updates

--- data/data.js
+++ data/data.js
@@ -1,13 +1,47 @@
 const data = [
   {
-    ageGroup: "Under 18",
-    name: "Coventent House Youth Crisis Program",
-    website:
-      "https://www.covenanthousebc.org/programs-services/housing/crisis-program/",
-    phoneNumber: "1-877-685-7474",
-    address: "1280 Seymour St, Vancouver, BC V6B 3N9",
-    description:
-      "At Covenant House’s Crisis Program, we welcome young people, aged 16 to 24, who are at risk of, or are experiencing, homelessness. Our first priority is to meet young people’s basic needs, a safe space to sleep, nutritious food, warm clothing and showers",
+    age: "Under 18",
+    resources: [
+      {
+        name: "Coventent House Youth Crisis Program",
+        website:
+          "https://www.covenanthousebc.org/programs-services/housing/crisis-program/",
+        phoneNumber: "1-877-685-7474",
+        address: "1280 Seymour St, Vancouver, BC V6B 3N9",
+        description:
+          "At Covenant House’s Crisis Program, we welcome young people, aged 16 to 24, who are at risk of, or are experiencing, homelessness. Our first priority is to meet young people’s basic needs, a safe space to sleep, nutritous food, warm clothing and showers",
+      },
+      {
+        name: "Directions Youth Haven",
+        website: "https://vancouver.pathwaysbc.ca/programs/555",
+        phoneNumber: "1-877-685-7474",
+        address: "1138 Burrard Street, Vancouver, BC V6Z 1Y7",
+        description:
+          "A low-barrier, temporary emergency residential program providing five beds for Vancouver youth ages 16 to 19 of all genders and orientations who are street involved and need a safe space to stay.",
+      },
+    ],
+  },
+  {
+    age: "Social Assistance No",
+    resources: [
+      {
+        name: "BC Income Assistance Application",
+        website:
+          "https://www2.gov.bc.ca/gov/content/family-social-supports/income-assistance/apply-for-assistance",
+        phoneNumber: "1-866-866-0800",
+        address: "180 Main St Vancouver BC V6A 3V5",
+        description: "Access Low income support in British Columbia by applying today",
+      },
+      {
+        name: "Gathering Place Community Centre",
+        website:
+          "https://vancouver.ca/parks-recreation-culture/gathering-place-location.aspx",
+        phoneNumber: "604-665-2391",
+        address: "609 Helmcken Street, Vancouver, BC V6B 5R1",
+        description:
+          "The Gathering Place Community Centre offers programs and services to the Downtown South community. We primarily serve vulnerable populations, including people on lower income, people with disabilities, seniors, people of diverse ethnic backgrounds, the LGBTQ community, youth, and people who are homeless.",
+      },
+    ],
   },
 ];
 
