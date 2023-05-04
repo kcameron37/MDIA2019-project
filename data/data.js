8715fa79ddefc18415245768d8fb683d6bf705f7
kcameron37 <115664906+kcameron37@users.noreply.github.com>
2023-05-04 12:14:46 -0700

    data updates

--- data/data.js
+++ data/data.js
@@ -6,7 +6,7 @@ const data = {
       phoneNumber: "1-877-685-7474",
       address: "1280 Seymour St, Vancouver, BC V6B 3N9",
       description:
-        "At Covenant House’s Crisis Program, we welcome young people, aged 16 to 24, who are at risk of, or are experiencing, homelessness. Our first priority is to meet young people’s basic needs, a safe space to sleep, nutritious food, warm clothing, and showers",
+        "At Covenant House Crisis Program, we welcome young people, aged 16 to 24, who are at risk of, or are experiencing, homelessness. Our first priority is to meet young people’s basic needs, a safe space to sleep, nutritious food, warm clothing, and showers",
     },
     {
       name: "Directions Youth Haven",
