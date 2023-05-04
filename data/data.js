53335a27b584bc0ac678942c1f9ace6bfcb68397
kcameron37 <115664906+kcameron37@users.noreply.github.com>
2023-05-04 11:46:22 -0700

    adding more data

--- data/data.js
+++ data/data.js
@@ -1,48 +1,92 @@
-const data = [
-  {
-    age: "Under 18",
-    resources: [
-      {
-        name: "Coventent House Youth Crisis Program",
-        website:
-          "https://www.covenanthousebc.org/programs-services/housing/crisis-program/",
-        phoneNumber: "1-877-685-7474",
-        address: "1280 Seymour St, Vancouver, BC V6B 3N9",
-        description:
-          "At Covenant House’s Crisis Program, we welcome young people, aged 16 to 24, who are at risk of, or are experiencing, homelessness. Our first priority is to meet young people’s basic needs, a safe space to sleep, nutritous food, warm clothing and showers",
-      },
-      {
-        name: "Directions Youth Haven",
-        website: "https://vancouver.pathwaysbc.ca/programs/555",
-        phoneNumber: "1-877-685-7474",
-        address: "1138 Burrard Street, Vancouver, BC V6Z 1Y7",
-        description:
-          "A low-barrier, temporary emergency residential program providing five beds for Vancouver youth ages 16 to 19 of all genders and orientations who are street involved and need a safe space to stay.",
-      },
-    ],
-  },
-  {
-    age: "Social Assistance No",
-    resources: [
+const data = {
+  under18: [
+    {
+      name: "Coventent House Youth Crisis Program",
+      website: "https://www.covenanthousebc.org/programs-services/housing/crisis-program/",
+      phoneNumber: "1-877-685-7474",
+      address: "1280 Seymour St, Vancouver, BC V6B 3N9",
+      description:
+        "At Covenant House’s Crisis Program, we welcome young people, aged 16 to 24, who are at risk of, or are experiencing, homelessness. Our first priority is to meet young people’s basic needs, a safe space to sleep, nutritious food, warm clothing, and showers",
+    },
+    {
+      name: "Directions Youth Haven",
+      website: "https://vancouver.pathwaysbc.ca/programs/555",
+      phoneNumber: "1-877-685-7474",
+      address: "1138 Burrard Street, Vancouver, BC V6Z 1Y7",
+      description:
+        "A low-barrier, temporary emergency residential program providing five beds for Vancouver youth ages 16 to 19 of all genders and orientations who are street involved and need a safe space to stay.",
+    },
+  ],
+  socialAssistanceNo: [
+    {
+      name: "BC Income Assistance Application",
+      website: "https://www2.gov.bc.ca/gov/content/family-social-supports/income-assistance/apply-for-assistance",
+      phoneNumber: "1-866-866-0800",
+      address: "180 Main St Vancouver BC V6A 3V5",
+      description: "Access Low-income support in British Columbia by applying today",
+    },
+    {
+      name: "Gathering Place Community Centre",
+      website: "https://vancouver.ca/parks-recreation-culture/gathering-place-location.aspx",
+      phoneNumber: "604-665-2391",
+      address: "609 Helmcken Street, Vancouver, BC V6B 5R1",
+      description:
+        "The Gathering Place Community Centre offers programs and services to the Downtown South community. We primarily serve vulnerable populations, including people on lower income, people with disabilities, seniors, people of diverse ethnic backgrounds, the LGBTQ community, youth, and people who are homeless.",
+    },
+  ],
+  food1OrLess: [
+    {
+      name: "Greater Vancouver Food Bank",
+      website: "https://foodbank.bc.ca/find-food/locations/",
+      phoneNumber: "604.876.3601",
+      address: "8345 Winston Street Burnaby, BC V5A 2H3",
+      description: "Providing Healthy Food to Those in Need.",
+    },
+    {
+      name: "The Dugout - Hot breakfast",
+      website: "https://www.thedugout.org/our-programs",
+      phoneNumber: "(604)685-5239",
+      address: "59 Powell St, Vancouver, BC V6A 1E9",
+      description:
+        "The Dugout provides a community living room where people can spend time off the streets",
+    },
+  ],
+  safePlaceToSleepNo: [
+    {
+      name: "Lookout Housing and Health Society'",
+      website: "https://lookoutsociety.ca/contact-us/",
+      phoneNumber: "604-255-0340",
+      address: "544 Columbia Street, New Westminster, B.C., V3L 1B1",
+      description:
+        "We offer a range of programs, housing, and health solutions to vulnerable adults living with multiple challenges.",
+    },
+
+    {
+      name: "Belkin House Vancouver",
+      website:"https://belkinhousesa.ca/",
+      phoneNumber: "(604) 681-3405",
+      address: "555 Homer Street, Vancouver",
+      description:
+        "We provide a clean, safe, secure, and comfortable accommodation for those who have come down on hard times.",
+    }, 
+  ],
+
+    incomeLevel18000OrOver: [
       {
         name: "BC Income Assistance Application",
-        website:
-          "https://www2.gov.bc.ca/gov/content/family-social-supports/income-assistance/apply-for-assistance",
+        website: "https://www2.gov.bc.ca/gov/content/family-social-supports/income-assistance/apply-for-assistance",
         phoneNumber: "1-866-866-0800",
         address: "180 Main St Vancouver BC V6A 3V5",
         description: "Access Low income support in British Columbia by applying today",
       },
-      {
-        name: "Gathering Place Community Centre",
-        website:
-          "https://vancouver.ca/parks-recreation-culture/gathering-place-location.aspx",
-        phoneNumber: "604-665-2391",
-        address: "609 Helmcken Street, Vancouver, BC V6B 5R1",
-        description:
-          "The Gathering Place Community Centre offers programs and services to the Downtown South community. We primarily serve vulnerable populations, including people on lower income, people with disabilities, seniors, people of diverse ethnic backgrounds, the LGBTQ community, youth, and people who are homeless.",
+    
+    {
+      name: "Here to help BC",
+      website: "https://www.heretohelp.bc.ca/q-and-a/how-do-i-apply-for-income-assistance",
+      phoneNumber: "604-669-7600",
+      address: "905-1130 West Pender St, Vancouver, BC, V6E 4A4",
+      description:
+      "Here to help will help you navigate through the Provincial income assistance, which has three income levels: basic assistance (also called welfare), Persons with Disabilities benefits (PWD), and the Persons with Persistent and Multiple Barriers to employment benefits (PPMB).",
       },
-    ],
-  },
-];
-
-export default data;
+  ],
+  };
